c0839d2eb4fabcdd552d4e32d9af5d5e9c2e4b66
brick <brick0@gmail.com>
2018-02-18 17:53:55 -0600

    aa

--- src/app/boxes/boxes.ts
+++ src/app/boxes/boxes.ts
@@ -3,6 +3,7 @@ import "../three-display/enableThreeExamples";
 import "three/examples/js/renderers/CSS3DRenderer"
 import {ThreeBSP} from "./ThreeCSG";
 import {Vector3} from "three";
+import * as dat from 'dat.gui/build/dat.gui.js';
 
 export interface ThreeDisplay {
   glRenderer: THREE.WebGLRenderer;
@@ -12,7 +13,12 @@ export interface ThreeDisplay {
   camera: THREE.Camera;
   ctxCanvas2D: CanvasRenderingContext2D;
   render: ()=>void;
+  labelUpdates:Label[];
+  refreshLabels:()=>void;
+  clickable:any[];
 }
+
+const U_SIZE=44.45;
 
 export class Box {
       o:any;                     // user data
@@ -21,36 +27,32 @@ export class Box {
       label:string;
       kind:string;
       labels:Label[] = [];
-      totalRotation = new THREE.Vector3(0,0,0); // placeholder to keep track of total rotations
+      rotation = new THREE.Vector3(0,0,0); // placeholder to keep track of total rotations
       specs:ObjectSpecs;
       isRoot:boolean = false;
       children:Box[]= [];
+      gui:dat.GUI=null;
 
   constructor(public display:ThreeDisplay, public id:number,  isRoot:boolean) {
-        this.group.userData=this;
-        if (isRoot){
-          this.isRoot=isRoot;
+      this.isRoot=isRoot!=undefined?isRoot:false;
+      this.group.userData=this;
+      if (this.isRoot){
           this.display.glScene.add(this.group)
-        }
+      }
   }
-
+  boxMoved() {
+    this.labels.forEach(l => {l.boxMoved()})
+    this.children.forEach(c=>c.boxMoved())
+  }
   addObject(o:Box){
-    o.updateTotalRotation(this.group.rotation.toVector3())
     this.group.add(o.group);
     this.children.push(o);
   }
-
   rotateY(angle){
+    this.rotation.add(new THREE.Vector3(0,angle,0))
     this.group.rotateY((angle*Math.PI)/180);
-    this.updateTotalRotation(this.group.rotation.toVector3())
-    this.children.forEach(c => c.updateTotalRotation(this.group.rotation.toVector3()))
-  }
+    this.boxMoved();
 
-  updateTotalRotation(v:THREE.Vector3){
-    this.totalRotation.add(v);
-    this.children.forEach(c => c.updateTotalRotation(this.group.rotation.toVector3()))
-    this.display.glScene.updateMatrixWorld(true); //need this to be able to copy to world pos
-    this.labels.forEach(l=>{ l.boxMoved()})
   }
 
   setHighlight = () => {
@@ -81,7 +83,7 @@ const defaultRackSpecs : RackSpecs= {
     back_rail: new THREE.Vector3( 100,  100, -400),
     width: 482.6 + 200,
     depth: 1000,
-    height: 100+42*44.45+100, // bottom+u_sizes+topspace
+    height: 100+42*U_SIZE+100, // bottom+u_sizes+topspace
     color: "#f221f2",
     position: new THREE.Vector3(0,0,0),
     rotation: 0,
@@ -103,7 +105,6 @@ class RackSpecs extends ObjectSpecs {
       this.height= this.front_rail.y+this.u_height*44.45+this.topSpace;
       this.color = o.color
     }else{
-      console.log("default specs")
       return defaultRackSpecs;
     }
   }
@@ -116,12 +117,12 @@ export class Rack extends Box {
     this.kind='rack';
     this.label=label;
     this.specs=new RackSpecs(o)
-    this.isRoot=true;
     this.buildShell()
     this.buildRails()
     this.buildLabels()
-
     this.group.add(this.box)
+    this.box.userData=this;
+    this.display.clickable.push(this.box);
   }
   buildShell(){
      let outter = new THREE.BoxGeometry(this.specs.width, this.specs.height, this.specs.depth, 1, 1, 1)
@@ -131,7 +132,7 @@ export class Rack extends Box {
      let r=outcsg.subtract(incsg)
      let geometry=r.toGeometry(r);
      this.box = new THREE.Mesh(geometry,
-                       new THREE.MeshLambertMaterial( {color: this.specs.color, wireframe:false }));
+       new THREE.MeshLambertMaterial( {color: this.specs.color, wireframe:false }));
 
   }
 
@@ -158,24 +159,72 @@ export class Rack extends Box {
       x.position.add(front_rail);
     }
   };
-
   buildLabels() {
-    this.labels.push(new Label(this,this.label,"top-front"));
-    this.labels.push(new Label(this,this.label,"top-back"));
+    this.labels.push(new Label(this,this.label,"top-front",{maxHeight:150}));
+    this.labels.push(new Label(this,this.label,"top-back",{maxHeight:150}));
     this.labels.push(new Label(this,this.label,"top-center"));
-    if (this.isRoot) {
-      console.log('making rail labels');
+  }
+  buildRailLabels() {
       this.group.children.forEach((x) => {
         if (x.name == 'rail') {
-          let labelText=x.userData.i;
-          if(x.userData.i<10){labelText=' '+x.userData.i;}
-          x.geometry.computeBoundingBox()
-          this.labels.push(new Label(this,labelText,"mid-center",
-            {mesh:x, position:x.position.clone().add(new THREE.Vector3(0,0,-1))}))
+          let labelText = x.userData.i;
+          if (x.userData.i < 10) {
+            labelText = ' ' + x.userData.i;
+          }
+          (x as any).geometry.computeBoundingBox()
+          this.labels.push(new Label(this, labelText, "mid-center",
+            {mesh: x, position: x.position.clone().add(new THREE.Vector3(0, 0, -1))}))
         }
       })
-    }
+  }
+  addDevice(o:Box,u_pos,front){
+    this.addObject(o);
+    o.group.position.setY(-(this.specs.height/2)+this.specs.front_rail.y+U_SIZE*u_pos+o.specs.height/2)
+    o.boxMoved();
+  }
+  selected(){
+    this.setHighlight();
+    this.buildGUI()
+  }
+  buildGUI(){
+    this.gui = new dat.GUI()
+    let g:any={};
+    let ge = this.gui.addFolder('General')
+    this.gui.add(this,'label').onFinishChange(this.setLabelText);
+    g.position=this.gui.addFolder('Position');
+    if(!this.isRoot){
+      g.position.add(this.specs,'rotation',-180,180).step(1).onChange(this.rotateToAngle);}
+  /*  g.fr = this.gui.addFolder('Front Rail Position');
+    g.fr.add(this.specs.front_rail,'x').onFinishChange(this.dimChanged);
+    g.fr.add(this.specs.front_rail,'y').onFinishChange(this.dimChanged);
+    g.fr.add(this.specs.front_rail,'z').onFinishChange(this.dimChanged);
 
+    g.fr = this.gui.addFolder('Back Rail Position');
+    g.fr.add(this.specs.back_rail,'x').onFinishChange(this.dimChanged);
+    g.fr.add(this.specs.back_rail,'y').onFinishChange(this.dimChanged);
+    g.fr.add(this.specs.back_rail,'z').onFinishChange(this.dimChanged);
+*/
+  }
+  rotateToAngle=(v)=>{
+    console.log(v)
+    let delta= this.rotation.y-v;
+    this.rotateY(-delta);
+    this.display.refreshLabels()
+  }
+  setLabelText=(v)=>{
+    console.log(v);
+    this.label=v;
+    this.labels.forEach(l=>l.setLabelText(v))
+  }
+  destroyGUI(){
+    if(this.gui!=null){
+      this.gui.destroy();
+      this.gui=null;
+    }
+  }
+  deselected(){
+    this.clearHighlight();
+    this.destroyGUI()
   }
 
 }
@@ -185,60 +234,170 @@ const defaultRackableDeviceSpecs : RackableDeviceSpecs= {
     u_height: 1,
     width: 482,
     depth: 800,
-    height: 44.45, // bottom+u_sizes+topspace
-    color: "#42c2f4",
+    height: 42.45, // one u -2mm to have a bit of space
+    color: "#4bf442",
     position: new THREE.Vector3(0,0,0),
     rotation: 0
 };
 class RackableDeviceSpecs extends ObjectSpecs {
 
-  constructor(o){
+  constructor(spec){
     super();
-    if(o!=undefined){
-      this.u_height=o.u_height;
-      this.height= this.u_height*44.45;
-      this.color = o.color
+    if(spec!=undefined){
+      this.u_height=spec.u_height;
+      this.height= this.u_height*44.45-2;
+      this.color = spec.color
     }else{
-      console.log("default specs")
       return defaultRackableDeviceSpecs;
     }
   }
 }
 
+export class RackableDevice extends Box {
+  specs: RackableDeviceSpecs;
 
-class Label {
+  constructor(display: ThreeDisplay, id: number, label: string, specs?: any, isRoot?: boolean) {
+    super(display, id, isRoot);
+    this.kind = 'rackable';
+    this.label = label;
+    this.specs = new RackableDeviceSpecs(specs)
+    this.isRoot = isRoot;
+    this.buildShell()
+    this.group.add(this.box)
+    this.box.userData=this;
+    this.display.clickable.push(this.box);
+  }
+
+  buildShell(){
+   let g = new THREE.BoxGeometry(this.specs.width, this.specs.height, this.specs.depth, 1, 1, 1)
+          this.box=new THREE.Mesh(g,
+              new THREE.MeshLambertMaterial( {color: this.specs.color, wireframe:false })
+              );
+  }
+
+  buildLabels() {
+    this.labels.push(new Label(this, this.label, "front"));
+    this.labels.push(new Label(this, this.label, "back"));
+    this.labels.forEach(l=>{l.boxRotated();l.boxMoved()})
+    this.display.labelUpdates.concat(this.labels);
+  }
+
+}
+
+
+const defaultRoomSpecs : RoomSpecs= {
+    u_height: 1,
+    width: 0, // doesn't matter
+    depth: 0, // doesn't matter
+    height: 5000, // one u
+    color: "#e3f7eb",
+    position: new THREE.Vector3(0,0,0),
+    rotation: 0,
+    footPrint:[
+      new THREE.Vector2(0,0),
+      new THREE.Vector2(10000,0),
+      new THREE.Vector2(10000,5000),
+      new THREE.Vector2(0,5000)
+    ]
+};
+class RoomSpecs extends ObjectSpecs {
+  footPrint:THREE.Vector2[];
+  constructor(spec){
+    super();
+    if(spec!=undefined){
+      this.height= this.u_height*44.45;
+      this.color = spec.color
+    }else{
+      return defaultRoomSpecs;
+    }
+  }
+}
+
+export class Room extends Box {
+  specs: RoomSpecs;
+  footPrintOnly=true;
+  constructor(display: ThreeDisplay, id: number, label: string, specs?: any, isRoot?: boolean) {
+    super(display, id, isRoot);
+    this.kind = 'room';
+    this.label = label;
+    this.specs = new RoomSpecs(specs)
+    this.isRoot = isRoot;
+    this.buildShell()
+    this.group.add(this.box)
+  }
+  buildShell(){
+    let shape = new THREE.Shape()
+            this.specs.footPrint.forEach((x,i)=>{
+                if(i==0){shape.moveTo(x.x,x.y)}
+                else{shape.lineTo(x.x,x.y)}
+            })
+    shape.lineTo(this.specs.footPrint[0].x,this.specs.footPrint[0].y)
+    let g = this.footPrintOnly? new THREE.ExtrudeGeometry(shape,{steps:1,amount:1}):
+                new THREE.ExtrudeGeometry(shape,{steps:1,amount:this.specs.height})
+    this.box=new THREE.Mesh(g,
+                new THREE.MeshBasicMaterial( { color: this.specs.color, side: THREE.DoubleSide,
+                wireframe:!this.footPrintOnly
+                } ));
+    this.box.rotateX((90 * Math.PI)/180)
+    this.box.position.set(0,-10,0)
+  }
+
+  addDevice(o:Box,pos:THREE.Vector3){
+    this.addObject(o);
+    if(pos.y==0) {
+      o.group.position.copy(pos);
+      o.group.position.add(new THREE.Vector3(0,o.specs.height/2+1,0))
+    }
+    o.boxMoved();
+  }
+}
+
+
+export class Label {
   static fontSize = 14;
   static font = '14px Helvetica';
-  hole; // we need to punch a hole in the css
+  hole=null; // we need to punch a hole in the css
   display: ThreeDisplay;
   element = document.createElement('div');
   cssText = new THREE.CSS3DObject(this.element)
   textWidth: number;
   scale:number;
+  firstTime=true;
   constructor(private o: Box, private text: string, private where: string,private options?:any) {
     this.text = text;
     this.display = o.display;
-    this.element.textContent = text
-    this.element.style.font = Label.font;
-    this.element.style.background = 'white';
     if(this.options==undefined){
       this.options={}
     }
     this.o.box.geometry.computeBoundingBox();
+    this.element.style.font = Label.font;
+    this.element.style.background = 'white';
+    this.setLabelText(text);
+    this.display.cssScene.add(this.cssText);
+    this.hole.userData={"kind":"hole"};
+  }
+
+  setLabelText(text:string){
+    this.element.textContent = text
+    this.element.textContent=text;
+    this.text=text;
     this.textWidth = this.getTextWidth(this.text);
     let geometry = new THREE.PlaneGeometry(this.textWidth, Label.fontSize);
-
+    if(this.hole!=null){
+      this.o.group.remove(this.hole)
+    }
     this.hole = new THREE.Mesh(geometry,
       new THREE.MeshLambertMaterial({color: 0x000000, opacity: 0.0, side: THREE.DoubleSide}));
+    if(this.where=='top-center') {
+      this.hole.rotateX((-90 * Math.PI) / 180)
+      if(this.firstTime){
+        this.cssText.rotateX((-90 * Math.PI) / 180)
+        this.firstTime=false;
+      }
+    }
     this.o.group.add(this.hole);
-    this.display.cssScene.add(this.cssText);
     this.setScale();
     this.setPosition();
-    this.setRotation();
-
-    this.hole.userData={"kind":"hole"};
-    this.display.glScene.updateMatrixWorld(true);
-
   }
 
   setPosition(){
@@ -263,26 +422,26 @@ class Label {
       if (this.where == 'mid-center') {
         this.hole.position.copy(new THREE.Vector3(0, 0, max.z))
       }
+      if(this.where =='front'){
+        this.hole.position.copy(new THREE.Vector3(0, 0, max.z+1))
+      }
+      if(this.where =='back'){
+        this.hole.position.copy(new THREE.Vector3(0, 0, min.z-1))
+      }
     }
     this.cssText.position.copy(this.hole.position)
     this.o.group.localToWorld(this.cssText.position)
   }
-  setRotation(){
-     if(this.where=='top-center') {
-      this.hole.rotateX((-90 * Math.PI) / 180)
-      this.cssText.rotateX((-90 * Math.PI) / 180)
-    }
-  }
+
   setScale(){
     let refObj=this.o.box;
     if('mesh' in this.options){refObj=this.options.mesh}
     let max = refObj.geometry.boundingBox.max;
     let min = refObj.geometry.boundingBox.min;
-
-    this.options.maxWidth = max.x - min.x ;
-    this.options.maxHeight = max.y - min.y ;
-    let scaleX=Math.abs(this.options.maxWidth/this.textWidth);
-    let scaleY=Math.abs(this.options.maxHeight/Label.fontSize);
+    let maxHeight=('maxHeight' in this.options)?this.options.maxHeight:max.y - min.y;
+    let maxWidth=('maxWidth' in this.options)?this.options.maxWidth:max.x - min.x;
+    let scaleX=Math.abs(maxWidth/this.textWidth);
+    let scaleY=Math.abs(maxHeight/Label.fontSize);
     this.scale= (scaleX<scaleY)?scaleX:scaleY;
     this.hole.scale.set(this.scale,this.scale,1);
     this.cssText.scale.copy(this.hole.scale)
@@ -291,9 +450,17 @@ class Label {
     this.display.ctxCanvas2D.font = Label.font;
     return this.display.ctxCanvas2D.measureText(text).width;
   }
-
-  boxMoved() {
-    let rotation=this.o.totalRotation
+  boxRotated(){
+    let rotation=new THREE.Vector3(0,0,0)
+    let parent=this.o.group;
+    console.log('-----------------------',this.o.kind)
+    while(parent!=null){
+      console.log(parent);
+      rotation.add(this.o.group.rotation.toVector3())
+      parent=parent.parent
+    }
+    console.log(rotation);
+    console.log(this.o.group.rotation)
     this.cssText.rotation.setFromVector3(rotation)
     if (this.where.includes('back')) {
       this.cssText.rotateY(Math.PI)
@@ -303,9 +470,21 @@ class Label {
       this.cssText.rotation.setFromVector3(v);
       this.cssText.rotateX((-90 * Math.PI) / 180)
     }
+    this.display.labelUpdates.push(this);
+  }
+  boxMoved() {
+    this.display.labelUpdates.push(this);
+  }
+  updatePosition(){ // rquires the world matrix to be updated
     this.cssText.position.copy(this.hole.position)
+    this.cssText.rotation.copy(this.hole.getWorldRotation())
     this.o.group.localToWorld(this.cssText.position)
   }
 
+  destroy(){
+    this.o.group.remove(this.hole);
+    this.display.cssScene.remove(this.cssText);
+    this.element.remove();
+  }
 
 }
--- src/app/three-display/three-display.component.ts
+++ src/app/three-display/three-display.component.ts
@@ -3,8 +3,8 @@ import * as THREE from 'three';
 import "./enableThreeExamples";
 import "three/examples/js/renderers/CSS3DRenderer"
 import "three/examples/js/controls/OrbitControls"
-
-import { ThreeDisplay,  Rack} from "../boxes/boxes"
+import * as dat from 'dat.gui'
+import {ThreeDisplay, Rack, RackableDevice,Label,Room} from "../boxes/boxes"
 
 @Component({
   selector: 'app-three-display',
@@ -20,17 +20,37 @@ export class ThreeDisplayComponent implements  ThreeDisplay {
   cssScene = new THREE.Scene();
   controls;
   camera = null;
+  raycast = new THREE.Raycaster();
+  mouse = new THREE.Vector2()
+  labelUpdates:Label[]=[];
+  room:Room;
+  clickable:any[]=[];
   ctxCanvas2D = document.createElement("canvas").getContext("2d");
-  rack:Rack;
   constructor() {
     this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 10000);
     this.camera.position.z = 5000;
-    this.rack=new Rack(this,1,'test12',null,true);
-    this.rack.label='this is a simpler test'
-    this.rack.isRoot=true
-    this.rack.rotateY(45)
+    this.room=new Room(this,0,"a room",null,true)
+    for(let j=0;j<2;j++) {
+        let rack=new Rack(this,1,'R'+j);
+        this.room.addDevice(rack,new THREE.Vector3(j*1000,0,0));
+        rack.rotateY(25);
+      for (let i = 0; i < 40; i++) {
+        let server = new RackableDevice(this, 2, "server" + i)
+        rack.addDevice(server, i, true);
+        server.buildLabels()
+      }
+    }
+
+    this.refreshLabels();
   }
 
+  refreshLabels(){
+    if(this.labelUpdates.length>0){
+      this.glScene.updateMatrixWorld(true)
+      this.labelUpdates.forEach(l=>l.updatePosition());
+      this.labelUpdates=[]
+    }
+  }
 
   createRender(domElement){
     let x= domElement.nativeElement.getBoundingClientRect()
@@ -48,23 +68,63 @@ export class ThreeDisplayComponent implements  ThreeDisplay {
     this.glScene.add(light);
     this.controls = new THREE.OrbitControls(this.camera, this.cssRenderer.domElement);
     this.controls.addEventListener('change', this.render)
+    this.glRenderer.domElement.addEventListener("mousedown", this.onMouseDown, false);
+    this.glRenderer.domElement.addEventListener("mouseup", this.onMouseClick, false);
 
   }
 
    ngAfterViewInit() {
         this.createRender(this.rendererContainer)
         this.render();
-    }
+   }
 
-    render = ()=> {
-        window.requestAnimationFrame(()=>this.render());
-        //this.mesh.rotation.x += 0.01;
-        //this.mesh.rotation.y += 0.02;
-        //this.rack.rotateY(2);
-        this.glRenderer.render(this.glScene, this.camera);
-        this.cssRenderer.render(this.cssScene, this.camera)
-    }
-
+   render = ()=> {
+     window.requestAnimationFrame(() => this.render());
+     //this.mesh.rotation.x += 0.01;
+     //this.mesh.rotation.y += 0.02;
+     //this.rack.rotateY(2);
+     this.glRenderer.render(this.glScene, this.camera);
+     this.cssRenderer.render(this.cssScene, this.camera)
+   }
+   onMouseDown = (event)=> {
+     this.mouse.x = event.offsetX;
+     this.mouse.y = event.offsetY;
+   }
+   selected:any;
+   onMouseClick = (event)=> {
+     console.log(event);
+     if (this.mouse.x == event.offsetX && this.mouse.y == event.offsetY) {
+       this.mouse.x = (event.offsetX / this.glRenderer.domElement.width) * 2 - 1
+       this.mouse.y = -(event.offsetY / this.glRenderer.domElement.height) * 2 + 1
+       this.raycast.setFromCamera(this.mouse, this.camera);
+       let intersects = this.raycast.intersectObjects(this.clickable, false);
+       console.log(intersects);
+       let clicked = intersects.reduce((r, x) => {
+         if (x.object.material.wireframe) {
+           return r
+         }
+         if (r && r.distance < x.distance) {
+           return r
+         }
+         return x
+       }, null);
+       if (!clicked && intersects.length > 0) {
+         clicked = intersects[0]
+       }
+       if (clicked) {
+         if (this.selected) {
+           if (this.selected.userData.deselected) {
+             this.selected.userData.deselected()
+           }
+         }
+         this.selected = clicked.object;
+         if (this.selected.userData.selected) {
+           this.selected.userData.selected()
+         }
+       }
+       this.render()
+     }
+   }
 }
 
 
